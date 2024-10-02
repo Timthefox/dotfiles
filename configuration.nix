f6bb722562736c2e835b39baac3dacd893149daf
Timfox <stephan.gollub@ewetel.net>
2024-10-02 14:32:55 +0200

    tastatur ausgelagert

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,7 @@ in
       ./hardware-configuration.nix
       ./hardware/nvidia.nix
       ./hardware/drucker.nix
+      ./hardware/tastatur.nix
     ];
 
   # Bootloader.
@@ -58,16 +59,6 @@ in
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "de";
-
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
