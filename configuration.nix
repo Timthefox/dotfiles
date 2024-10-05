603bf7ec4c04fcf1d33c9fc3237ba6e9fdcaf281
Timfox <stephan.gollub@ewetel.net>
2024-10-05 16:45:16 +0200

    sound ausgelagert

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
       ./hardware/drucker.nix
       ./hardware/tastatur.nix
       ./hardware/festplatten.nix
+      ./hardware/sound.nix
     ];
 
   # Bootloader.
@@ -44,21 +45,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
   environment.etc."paperless-admin-pass".text = "admin";
   services.paperless = {
     enable = true;
