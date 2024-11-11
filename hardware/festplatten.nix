0eccc3c3996ccad695857ec67f3b143d18cc8439
Timfox <stephan.gollub@ewetel.net>
2024-11-11 18:26:49 +0100

    Weitere vohandene Festplatten beim booten mounten.

--- hardware/festplatten.nix
+++ hardware/festplatten.nix
@@ -14,9 +14,26 @@ in {
     "ntfs"
   ];
   # TODO: bei Einrichtung für weitere Rechner muss die festplatte angepasst werden
-  fileSystems."/mnt/games" = {
-    device = "/dev/disk/by-uuid/0a21bdfb-6a38-4639-9481-d6098e542a9a";
-    fsType = "ext4";
-    options = mountOptions ++ ["defaults" "exec"];
+  fileSystems = {
+    "/mnt/games" = {
+      device = "/dev/disk/by-uuid/0a21bdfb-6a38-4639-9481-d6098e542a9a";
+      fsType = "ext4";
+      options = mountOptions ++ ["defaults" "exec"];
+    };
+    "/mnt/HDD_1TB" = {
+      device = "/dev/disk/by-uuid/700A97090A96CB8A";
+      fsType = "ntfs";
+      options = mountOptions;
+    };
+    "/mnt/SSD_SanDisk_1TB" = {
+      device = "/dev/disk/by-uuid/1E1C16581C162AF3";
+      fsType = "ntfs";
+      options = mountOptions;
+    };
+    "/mnt/Windows10" = {
+      device = "/dev/disk/by-uuid/D2A2E9C9A2E9B1E1";
+      fsType = "ntfs";
+      options = mountOptions;
+    };
   };
 }
