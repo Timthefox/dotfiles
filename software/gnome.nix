641c05b70aad8b8389c2f588b4768c9a926c936f
Timfox <stephan.gollub@ewetel.net>
2025-01-27 22:39:41 +0100

    autostart erstellt mit signal u vesktop

--- software/gnome.nix
+++ software/gnome.nix
@@ -29,18 +29,39 @@
     snapshot #webcam, die zZ gar nicht vorhanden ist
   ];
 
-  home-manager.users.${username}.dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      clock-show-seconds = true;
-      clock-show-weekday = true;
-      enable-hot-corners = false;
+  home-manager.users.${username} = {
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        clock-show-seconds = true;
+        clock-show-weekday = true;
+        enable-hot-corners = false;
+      };
+      "org/gnome/shell".app-picker-layout = "[]"; # sort apps by name
+      "org/gnome/mutter" = {
+        edge-tiling = true;
+        dynamic-workspaces = true;
+        workspaces-only-on-primary = true;
+      };
+      "org/gnome/shell/app-switcher".current-workspace-only = false;
     };
-    "org/gnome/shell".app-picker-layout = "[]"; # sort apps by name
-    "org/gnome/mutter" = {
-      edge-tiling = true;
-      dynamic-workspaces = true;
-      workspaces-only-on-primary = true;
-    };
-    "org/gnome/shell/app-switcher".current-workspace-only = false;
+    home.file = let
+      autostartPrograms = with pkgs; [
+        vesktop
+        signal-desktop
+      ];
+    in
+      builtins.listToAttrs (map
+        (pkg: {
+          name = ".config/autostart/" + pkg.pname + ".desktop";
+          value =
+            if pkg ? desktopItem
+            then {
+              text = pkg.desktopItem.text;
+            }
+            else {
+              source = pkg + "/share/applications/" + pkg.pname + ".desktop";
+            };
+        })
+        autostartPrograms);
   };
 }
